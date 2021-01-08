bbae3aa31df3f88e8e5003449ae644979bc5c7f3
Maria <maria@2-faces.ru>
2021-01-08 17:22:19 +0300

    Update SQL scripts

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,6 @@
 DELETE
+FROM user_roles;
+DELETE
 FROM users;
 DELETE
 FROM restaurants;
@@ -14,12 +16,18 @@ VALUES ('User1', '1@mail.ru', '111'),
        ('User2', '2@mail.ru', '222'),
        ('Admin', '3@mail.ru', '333');
 
+INSERT INTO user_roles (role, user_id)
+VALUES ('USER', 100000),
+       ('USER', 100001),
+       ('ADMIN', 100002),
+       ('USER', 100002);
+
 INSERT INTO RESTAURANTS(name, phone, address, website)
 VALUES ('Fish House', '+7(999)111-22-33', 'Moscow, Mayakovskaya, 10', 'www.fishhouse.ru'),
        ('Мясо', '+7(495)111-23-45', 'Moscow, Sretenka, 2', 'www.meat.ru'),
        ('Coffee shop', '+7(499)125-34-23', 'Moscow, Tulskaya, 17', 'www.coffee.ru'),
        ('Masterskaya', '+7(495)975-45-23', 'Moscow, Myasnitskaya, 22', 'www.masterskaya.ru'),
-       ('Bad rest', '+7(495)975-45-23', 'Moscow, Nitskaya, 13', 'www.bad.ru');
+       ('Bad rest', '+7(495)975-46-23', 'Moscow, Nitskaya, 13', 'www.bad.ru');
 
 INSERT INTO MENU(rest_id)
 VALUES (100003),
@@ -43,31 +51,3 @@ INSERT INTO VOTES (rest_id, menu_id, user_id)
 VALUES (100003, 100008, 100000),
        (100006, 100011, 100001),
        (100006, 100011, 100002);
-
-/*INSERT INTO LUNCHES(DATE, REST_ID)
-VALUES (  )*/
-
-/*DELETE FROM user_roles;
-DELETE FROM meals;
-DELETE FROM users;
-ALTER SEQUENCE global_seq RESTART WITH 100000;
-
-INSERT INTO users (name, email, password, calories_per_day)
-VALUES ('User', 'user@yandex.ru', '{noop}password', 2005),
-       ('Admin', 'admin@gmail.com', '{noop}admin', 1900);
-
-INSERT INTO user_roles (role, user_id)
-VALUES ('USER', 100000),
-       ('ADMIN', 100001),
-       ('USER', 100001);
-
-INSERT INTO meals (date_time, description, calories, user_id)
-VALUES ('2020-01-30 10:00:00', 'Завтрак', 500, 100000),
-       ('2020-01-30 13:00:00', 'Обед', 1000, 100000),
-       ('2020-01-30 20:00:00', 'Ужин', 500, 100000),
-       ('2020-01-31 0:00:00', 'Еда на граничное значение', 100, 100000),
-       ('2020-01-31 10:00:00', 'Завтрак', 500, 100000),
-       ('2020-01-31 13:00:00', 'Обед', 1000, 100000),
-       ('2020-01-31 20:00:00', 'Ужин', 510, 100000),
-       ('2020-01-31 14:00:00', 'Админ ланч', 510, 100001),
-       ('2020-01-31 21:00:00', 'Админ ужин', 1500, 100001);*/
--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,12 @@
 DROP SCHEMA IF EXISTS lunchroom;
 
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS dishes;
+DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS votes;
+DROP TABLE IF EXISTS users;
+
 CREATE SCHEMA lunchroom;
 
 DROP SEQUENCE IF EXISTS GLOBAL_SEQ;
@@ -8,21 +15,30 @@ CREATE SEQUENCE GLOBAL_SEQ START WITH 100000;
 
 CREATE TABLE users
 (
-    id       INTEGER   DEFAULT GLOBAL_SEQ.nextval PRIMARY KEY,
-    name     VARCHAR(25)             NOT NULL,
-    email    VARCHAR(100)            NOT NULL,
-    password VARCHAR(500)            NOT NULL,
-    date     TIMESTAMP DEFAULT now() NOT NULL,
-    enabled  BOOL      DEFAULT TRUE  NOT NULL
+    id       INTEGER DEFAULT GLOBAL_SEQ.nextval PRIMARY KEY,
+    name     VARCHAR(25)                  NOT NULL,
+    email    VARCHAR(100)                 NOT NULL,
+    password VARCHAR(500)                 NOT NULL,
+    date     DATE    DEFAULT CURRENT_DATE NULL,
+    enabled  BOOL    DEFAULT TRUE         NOT NULL
+);
+CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE user_roles
+(
+    user_id INTEGER NOT NULL,
+    role    VARCHAR,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE restaurants
 (
     id      INTEGER DEFAULT GLOBAL_SEQ.nextval PRIMARY KEY,
-    name    VARCHAR(50)  NOT NULL,
-    phone   VARCHAR(16)  NOT NULL,
-    address VARCHAR(300) NOT NULL,
-    website VARCHAR(100) NOT NULL
+    name    VARCHAR(50)        NOT NULL,
+    phone   VARCHAR(16) UNIQUE NOT NULL,
+    address VARCHAR(300)       NOT NULL,
+    website VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE menu
@@ -36,9 +52,9 @@ CREATE TABLE menu
 CREATE TABLE dishes
 (
     id      INTEGER DEFAULT GLOBAL_SEQ.nextval PRIMARY KEY,
-    name    VARCHAR(100)                 NOT NULL,
-    price   INTEGER                      NOT NULL,
-    menu_id INTEGER                      NOT NULL,
+    name    VARCHAR(100) NOT NULL,
+    price   INTEGER      NOT NULL,
+    menu_id INTEGER      NOT NULL,
     FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE
 );
 
@@ -52,37 +68,4 @@ CREATE TABLE votes
     FOREIGN KEY (rest_id) REFERENCES restaurants (id) ON DELETE CASCADE,
     FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-
 );
-
-/*
-CREATE TABLE users
-(
-    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    name             VARCHAR                           NOT NULL,
-    email            VARCHAR                           NOT NULL,
-    password         VARCHAR                           NOT NULL,
-    registered       TIMESTAMP           DEFAULT now() NOT NULL,
-    enabled          BOOL                DEFAULT TRUE  NOT NULL,
-    calories_per_day INTEGER             DEFAULT 2000  NOT NULL
-);
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
-
-CREATE TABLE user_roles
-(
-    user_id INTEGER NOT NULL,
-    role    VARCHAR,
-    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE TABLE meals
-(
-    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    user_id     INTEGER   NOT NULL,
-    date_time   TIMESTAMP NOT NULL,
-    description TEXT      NOT NULL,
-    calories    INT       NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (user_id, date_time);*/
