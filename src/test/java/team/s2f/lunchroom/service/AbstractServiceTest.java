1abf22fa4f1c7d141eb1afb36aadf006701ef813
Maria <maria@2-faces.ru>
2021-01-14 17:24:52 +0300

    Add DishServiceTest

---
+++ src/test/java/team/s2f/lunchroom/service/AbstractServiceTest.java
@@ -0,0 +1,31 @@
+package team.s2f.lunchroom.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import team.s2f.lunchroom.TimingExtension;
+import team.s2f.lunchroom.util.ValidationUtil;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringJUnitConfig(locations = {
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+//@ExtendWith(SpringExtension.class)
+@Sql(scripts = "classpath:db/data.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ExtendWith(TimingExtension.class)
+public abstract class AbstractServiceTest {
+
+    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
+    protected <T extends Throwable> void validateRootCause(Runnable runnable, Class<T> rootExceptionClass) {
+        assertThrows(rootExceptionClass, () -> {
+            try {
+                runnable.run();
+            } catch (Exception e) {
+                throw ValidationUtil.getRootCause(e);
+            }
+        });
+    }
+}
